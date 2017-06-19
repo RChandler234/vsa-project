e1c9790ffc77ea9f01d162a2a7a1008bec542487
RChandler234 <reid.c.chandler@gmail.com>
2017-06-19 11:26:44 -0500

    Add files via upload

---
+++ proj01.py
@@ -0,0 +1,26 @@
+# Name: Reid Chandler
+# Date: 6/19/17
+
+# proj01: A Simple Program
+# This program asks the user for his/her name and age.
+# Then, it prints a sentence that says when the user will turn 100.
+
+# If you complete extensions, describe your extensions here!
+
+name=raw_input("Enter your name: ")
+age= int(raw_input("Enter your age: "))
+stat= " will turn 100 in the year "
+Bday=raw_input("Have you had your birthday this year yet?: ")
+print Bday
+if Bday == yes or Yes:
+    yrs= 100-age
+else:
+    yrs= 99-age
+Fut= str(yrs+2017)
+first_letter = name[0].upper()
+other_letters = name[1:].lower()
+
+Name= first_letter+other_letters
+
+
+print Name+stat+Fut
