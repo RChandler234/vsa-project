031c8c58bcdec86df7f682e7cb5c2601dea41d09
RChandler234 <reid.c.chandler@gmail.com>
2017-06-20 14:37:07 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name: Reid Chandler
+# Date: 6/20/17
 
 # proj02_02: Fibonaci Sequence
 
@@ -10,11 +10,50 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 
+"""
+#Fibonacci Sequence (For Loop)
+x= int(raw_input("How many Fibonacci numbers to generate?: "))
+a=0
+b=1
+for number in range (1,x+1):
+    if number== 1:
+        FN=a+b
+        print FN
+    else:
+        FN=a+b
+        print FN
+        a=b
+        b=FN
+"""
+"""
+#Fibonacci Sequence (While Loop)
+x= int(raw_input("How many Fibonacci numbers do you want?: "))
+a=0
+b=1
+number=1
+while number < x+1:
+    if number == 1:
+        FN=a+b
+        print FN
+        number=number+1
+    else:
+        FN=a+b
+        print FN
+        a=b
+        b=FN
+        number=number+1
+"""
+""""
+#Powers of 2
+x= int(raw_input("How many powers of 2 do you want?: "))
 
-a= int(raw_input("How many Fibonacci numbers to generate?: "))
-x=1
-for number in (1,a+1):
-    FN=number+x+(y-x)
-    x=y
-    y=FN
-print FN
+for number in range (1,x+1):
+    power= 2 ** number
+    print power
+"""
+#Divisors
+x=int(raw_input("What number do you want the divisors of?: "))
+for divisor in range (1, x+1):
+        z=x%divisor
+        if z==0:
+            print divisor
--- proj03/proj03.py
+++ proj03/proj03.py
@@ -1,15 +1,43 @@
-# Name:
-# Date:
+# Name: Reid Chandler
+# Date: 6/20/17
 
 
 """ 
 proj 03: Guessing Game
-
+s
 Generate a random number between 1 and 9 (including 1 and 9). 
 Ask the user to guess the number, then tell them whether they guessed too low, too high, 
 or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+import random
 
+y= "yes"
+while y== "yes" or "yeah" or "ye" or "yep":
+    guess=0
+    variable_name=random.randint(1,9)
+    x=int(raw_input("Guess a number between 1 and 9: "))
+    while x != variable_name:
+        if guess!=2:
+            if x>variable_name:
+                print "Too big."
+                x = int(raw_input("Guess again: "))
+                guess=guess+1
+            elif x<variable_name:
+                print "Too small."
+                x = int(raw_input("Guess again: "))
+                guess=guess+1
+        else:
+            print "Sorry, you lost. The correct answer was" , variable_name
+            y = raw_input("Want to try again?: ")
+            y=y.lower()
+            guess = 0
+            break
 
+    if x== variable_name:
+        print "Congratulations," , variable_name , "is correct"
+        print "You took" , guess+1 , "turns"
+        y= raw_input("Do you want to keep going?(yes or no): ")
+    if y == "no" or "nah" or "nope":
+        print "The End"
