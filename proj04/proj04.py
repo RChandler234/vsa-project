94f07aca1f64e41d4e7101b09659bc8ed45b5e12
RChandler234 <reid.c.chandler@gmail.com>
2017-06-20 15:29:01 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name:Reid Chandler
+# Date:6/20/17
 
 """
 proj04
@@ -8,3 +8,22 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+
+user= raw_input("Write a string: ")
+user_input=user
+user=user.lower()
+user_list= []
+reverse_list=[]
+user=user.replace(" ","")
+for letter in user:
+    user_list.append(letter)
+    mylist=user_list
+x= len(user_list)
+for letter in range (1,x+1):
+    reverse_list.append(user_list[-1])
+    user_list= user_list[:-1]
+user_input=user_input[0].upper()+user_input[1:].lower()
+if mylist==reverse_list:
+    print user_input , "is a palindrome."
+else:
+    print user_input, "is not a palindrome."
